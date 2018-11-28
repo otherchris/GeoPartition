eb0353c77a4049ba95e9e55c6ae7468a98e80d2c
Chris Caragianis <chriscaragianis@gmail.com>
2018-11-28 17:02:18 -0500

    make release

--- mix.exs
+++ mix.exs
@@ -8,7 +8,14 @@ defmodule GeoPartition.MixProject do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: ["lib", "test/support"]
+      elixirc_paths: ["lib", "test/support"],
+      description: "Decompose polygons into polygons smaller than a given area",
+      source_url: "https://github.com/otherchris/GeoPartition",
+      package: [
+        name: "geo_partition",
+        licenses: ["MIT"],
+        links: %{"GitHub" => "https://github.com/otherchris/GeoPartition"}
+      ]
     ]
   end
 
